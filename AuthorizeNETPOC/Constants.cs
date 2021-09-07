453528642587f0dacc5e915efd738590739bfdd3
Param Thakkar <param.thakkar@ftxinfotech.com>
2021-09-07 12:50:25 +0530

    Added api configs constant

---
+++ AuthorizeNETPOC/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizeNETPOC
+{
+    public class Constants
+    {
+        public const string ApiLoginID = "5KP3u95bQpv";
+        public const string ApiTransactionKey = "346HZ32z3fP4hTG2";
+    }
+}

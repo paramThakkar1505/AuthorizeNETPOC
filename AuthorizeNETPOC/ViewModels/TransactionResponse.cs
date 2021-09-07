fed460e179ec4e2d1be3b1c9b1c5689529fb0851
Param Thakkar <param.thakkar@ftxinfotech.com>
2021-09-07 13:18:38 +0530

    Transaction Response added

---
+++ AuthorizeNETPOC/ViewModels/TransactionResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizeNETPOC.ViewModels
+{
+    public class TransactionResponse
+    {
+        public string Note { get; set; } //message returned from PaymentGateway
+        public string AuthorizationCode { get; set; } //auth code 
+        public string CardType { get; set; } //Visa or Mastercard etc
+        public string ReferenceNumber { get; set; }
+        public string ResponseCode { get; set; } 
+        public string ResponseMessage { get; set; }
+        public string ServiceName { get; set; } //Name of PaymentGateway
+        public string TransactionStatus { get; set; }
+        public string TransactionId { get; set; }
+    }
+}
